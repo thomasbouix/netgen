bd5f0dcb0eeed42c378eae2a56207aeb225f737c
Thomas <thomas.bouix1@gmail.com>
2021-08-30 10:50:37 +0200

    fix(generic_layer) : inputs are processed every 2 clock cycle

--- hw/ips/generic_layer/src/generic_layer.vhd
+++ hw/ips/generic_layer/src/generic_layer.vhd
@@ -19,7 +19,7 @@ entity generic_layer is
         clk             : in  std_logic;
         rstn            : in  std_logic;
         inputs          : in  std_logic_vector( (G_NB_INPUTS  * DATA_WIDTH) - 1 downto 0);
-        outputs         : out std_logic_vector( (G_NB_WEIGHTS * DATA_WIDTH) - 1 downto 0) 
+        outputs         : out std_logic_vector( (G_NB_WEIGHTS * DATA_WIDTH) - 1 downto 0)   := (others => '0')
     );
 
 end entity;
@@ -27,64 +27,72 @@ end entity;
 -----------------------------------
 
 architecture rtl of generic_layer is
-    
-    type t_parameters is array (0 to G_NB_WEIGHTS-1) of T_DATA;
 
+    type T_SM           is (ADDING_INPUTS, COMPUTING_OUTPUTS);
+    type t_parameters   is array (0 to G_NB_WEIGHTS-1) of T_DATA;
+
+    signal r_sm         : T_SM              := ADDING_INPUTS;
     signal weights      : t_parameters      := (others => (0 => '1', others => '0'));   -- weights[i] = 1
     signal offsets      : t_parameters      := (others => (0 => '1', others => '0'));   -- offsets[i] = 1
     signal inputs_sum   : integer           := 0; 
 
 begin
 
-    -- res = i(0) + i(1) + ... + i(n-1)
-    p_inputs_sum : process(clk) is
+    -- outputs are computed in two clock cycles to prevent timing errors
+    p_outputs : process(clk) is
    
         variable res    : integer   :=  0 ;
-        variable done   : std_logic := '0'; 
     
     begin
 
-        if rstn = '0' then
-            res     :=  0 ;
-            done    := '0';
+        if rising_edge(clk) then
             
-        elsif rising_edge(clk) then
+            if rstn = '0' then
 
-            if done = '0' then
-                done := '1';
+                res     :=  0 ;
 
-                adding_inputs : for i in 0 to G_NB_INPUTS-1 loop
-                    res := res + to_integer( signed( inputs(((i*DATA_WIDTH)+DATA_WIDTH-1) downto i*DATA_WIDTH))); 
-                end loop;
+                weights <= (others => (0 => '1', others => '0') );
+                offsets <= (others => (0 => '1', others => '0') );
+                outputs <= (others => '0');
 
-                inputs_sum <= res;
+                r_sm    <= ADDING_INPUTS;
 
+            else 
+
+                case r_sm is
+
+                    when ADDING_INPUTS =>           
+
+                        res     := 0;
+                        -- res = i(0) + i(1) + ... + i(n-1)
+                        loop_adding_inputs : for i in 0 to G_NB_INPUTS-1 loop
+                            res := res + to_integer( signed( inputs(((i*DATA_WIDTH)+DATA_WIDTH-1) downto i*DATA_WIDTH))); 
+                        end loop;
+
+                        inputs_sum  <= res;
+                        r_sm        <= COMPUTING_OUTPUTS;
+
+                    when COMPUTING_OUTPUTS =>       
+                        
+                        -- outputs[i] = ( weights[i] * inputs_sum ) + ( NB_INPUTS * offsets[i] )
+                        loop_computing_outputs : for i in 0 to G_NB_WEIGHTS-1 loop
+
+                            outputs(i*DATA_WIDTH + DATA_WIDTH - 1 downto i*DATA_WIDTH) <= 
+                                std_logic_vector(to_signed(
+                                    to_integer(weights(i)) * inputs_sum + (G_NB_INPUTS * to_integer(offsets(i)))
+                                , DATA_WIDTH));
+
+                        end loop;
+
+                        r_sm       <= ADDING_INPUTS;
+
+                    when others =>
+                        r_sm       <= ADDING_INPUTS;
+
+                end case;
             end if;
         end if;
     end process;
-
-    p_outputs_computing : process(clk) begin
-
-        if (rstn = '0') then
-
-            weights <= (others => (0 => '1', others => '0') );
-            offsets <= (others => (0 => '1', others => '0') );
-            outputs <= (others => '0');
-
-        elsif rising_edge(clk) then
-
-            -- outputs[i] = ( weights[i] * inputs_sum ) + ( NB_INPUTS * offsets[i] )
-            processing_outputs : for i in 0 to G_NB_WEIGHTS-1 loop
-
-               outputs(i*DATA_WIDTH + DATA_WIDTH - 1 downto i*DATA_WIDTH) <= std_logic_vector(to_signed(
-                                                                                        to_integer(weights(i)) * inputs_sum + 
-                                                                                        (G_NB_INPUTS * to_integer(signed(offsets(i))))
-                                                                             , DATA_WIDTH));
-            end loop;
-
-        end if;
-
-    end process; 
 
 end architecture;
 
