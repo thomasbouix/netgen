707311d8928de3715946cbf8d63c1e9bf994a484
Thomas <thomas.bouix1@gmail.com>
2021-10-03 12:43:36 +0200

    fix(generic_layer) : changing computational behavior
    dev(generic_layer) : simulation ongoing
    
    dev(generic_layer) : fixing test bench error

--- hw/ips/generic_layer/src/generic_layer.vhd
+++ hw/ips/generic_layer/src/generic_layer.vhd
@@ -24,7 +24,6 @@ entity generic_layer is
         ----------------------------------------------------
         ------------------- layer I/O ----------------------
         ----------------------------------------------------
-
         inputs              : in  std_logic_vector( g_NB_INPUTS  * p_DATA_WIDTH - 1 downto 0);
         outputs             : out std_logic_vector( g_NB_OUTPUTS * p_DATA_WIDTH - 1 downto 0) := (others => '0');
 
@@ -42,32 +41,32 @@ end entity;
 
 architecture rtl of generic_layer is
 
-    signal r_weights            : t_int_array(0 to g_NB_OUTPUTS)            := (others => 1);       -- weights[i] = 1
-    signal r_offsets            : t_int_array(0 to g_NB_OUTPUTS)            := (others => 0);       -- offsets[i] = 0
+    signal r_weights    : t_weights(0 to g_NB_OUTPUTS - 1, 0 to g_NB_INPUTS - 1)    := (others => (others => 1));   -- weights[i][j] = 1
+    signal r_bias       : t_bias   (0 to g_NB_OUTPUTS - 1)                          := (others => 0);               -- bias[i] = 0
 
-    signal r_inputs             : t_data_array(0 to g_NB_INPUTS  - 1);                              -- facilitate I/O manipulation   
-    signal r_outputs            : t_data_array(0 to g_NB_OUTPUTS - 1);                              -- facilitate I/O manipulation   
+    signal r_inputs     : t_int_array(0 to g_NB_INPUTS  - 1);                                                       -- facilitates I/O manipulation   
+    signal r_outputs    : t_int_array(0 to g_NB_OUTPUTS - 1);                                                       -- facilitates I/O manipulation   
 
     -- ip address space ( 1 addr / data )
-    constant c_WEIGHTS_MEM_BASE : integer                                   := g_MEM_BASE;
-    constant c_WEIGHTS_MEM_END  : integer                                   := g_MEM_BASE + g_NB_OUTPUTS;  
-    constant c_OFFSETS_MEM_BASE : integer                                   := g_MEM_BASE + g_NB_OUTPUTS + 1;
-    constant c_OFFSETS_MEM_END  : integer                                   := g_MEM_BASE + g_NB_OUTPUTS + 1 + g_NB_OUTPUTS;
+    constant c_WEIGHTS_MEM_BASE : integer   := g_MEM_BASE;
+    constant c_WEIGHTS_MEM_END  : integer   := g_MEM_BASE + g_NB_INPUTS * g_NB_OUTPUTS - 1;  
+    constant c_BIAS_MEM_BASE    : integer   := g_MEM_BASE + g_NB_INPUTS * g_NB_OUTPUTS;
+    constant c_BIAS_MEM_END     : integer   := g_MEM_BASE + g_NB_INPUTS * g_NB_OUTPUTS + g_NB_OUTPUTS - 1;
 
 begin
 
-    -- combinatory process for type conversion
+    -- combinatory process for I/O type conversion
     p_io_conversion : process(inputs, r_outputs) is 
     
     begin
 
         for i in 0 to g_NB_INPUTS - 1 loop
-            r_inputs(i) <= signed(inputs(p_DATA_WIDTH * (g_NB_INPUTS - i) - 1 downto p_DATA_WIDTH * (g_NB_INPUTS - i - 1)));
+            r_inputs(i) <= to_integer(signed(inputs(p_DATA_WIDTH * (g_NB_INPUTS - i) - 1 downto p_DATA_WIDTH * (g_NB_INPUTS - i - 1))));
         end loop;
 
 
         for i in 0 to g_NB_OUTPUTS - 1 loop
-            outputs(p_DATA_WIDTH * (g_NB_OUTPUTS - i) - 1 downto p_DATA_WIDTH * (g_NB_OUTPUTS - i - 1)) <= std_logic_vector(r_outputs(i));
+            outputs(p_DATA_WIDTH * (g_NB_OUTPUTS - i) - 1 downto p_DATA_WIDTH * (g_NB_OUTPUTS - i - 1)) <= std_logic_vector(to_signed(r_outputs(i), p_DATA_WIDTH));
         end loop;
 
     end process;
@@ -76,8 +75,10 @@ begin
     p_configuration : process(clk) is
 
         variable v_weight_addr  : std_logic := '0';     -- configuring a weight
-        variable v_offset_addr  : std_logic := '0';     -- configuring an offset
+        variable v_bias_addr    : std_logic := '0';     -- configuring a bias
         variable v_write_addr   : integer   :=  0 ;     -- configuration addr as an integer
+        variable v_weight_i     : integer   :=  0 ;     -- index of weight row 
+        variable v_weight_j     : integer   :=  0 ;     -- index of weight column 
 
     begin
 
@@ -87,10 +88,17 @@ begin
 
                 v_write_addr    :=  0 ;
                 v_weight_addr   := '0';
-                v_offset_addr   := '0';
+                v_bias_addr     := '0';
 
-                r_weights       <= (others => 1);
-                r_offsets       <= (others => 0);
+                v_weight_i      :=  0;
+                v_weight_j      :=  0;
+
+                -- PUT BACK TO ZERO AFTER TESTS !!
+                r_weights       <= ( others => ( others => 1));
+--              r_weights       <= ( 0      => ( 1 => 2, others => 1),
+--                                   others => (         others => 1)); 
+
+                r_bias          <= (others => 1);           
 
             else 
 
@@ -100,19 +108,21 @@ begin
 
                     v_weight_addr   := '1';
 
-                elsif v_write_addr >= c_OFFSETS_MEM_BASE and v_write_addr <= c_OFFSETS_MEM_END then     -- configuring an offset
+                elsif v_write_addr >= c_BIAS_MEM_BASE and v_write_addr <= c_BIAS_MEM_END then           -- configuring a bias
 
-                    v_offset_addr   := '1';
+                    v_bias_addr   := '1';
 
                 end if;
 
                 if v_weight_addr = '1' then
 
-                    r_weights(v_write_addr - g_MEM_BASE)  <= to_integer(signed(cfg_data)); 
+                    v_weight_i                          := (to_integer(signed(cfg_addr)) - g_MEM_BASE) /   g_NB_INPUTS; 
+                    v_weight_j                          := (to_integer(signed(cfg_addr)) - g_MEM_BASE) mod g_NB_INPUTS; 
+                    r_weights(v_weight_i, v_weight_j)   <=  to_integer(signed(cfg_data)); 
 
-                elsif v_offset_addr = '1' then
+                elsif v_bias_addr = '1' then
 
-                    r_offsets(v_write_addr - g_MEM_BASE)  <= to_integer(signed(cfg_data)); 
+                    r_bias(v_write_addr - g_MEM_BASE)  <=  to_integer(signed(cfg_data)); 
 
                 end if;
 
@@ -124,30 +134,36 @@ begin
 
 
     p_processing_outputs : process(clk) is
+
+        variable v_res  : integer := 0;
    
-        variable v_inputs_sum : integer :=  0 ;    -- sum of all inputs
-    
     begin
 
         if rising_edge(clk) then
             
             if rstn = '0' then
 
-                v_inputs_sum        :=  0 ;
-                r_outputs           <= (others => (others => '0'));
+                v_res               := 0;
+                r_outputs           <= (others => 0);
 
             else 
 
-                v_inputs_sum        := 0;
+                v_res := 0;
 
-                -- v_inputs_sum = i(0) + i(1) + ... + i(n-1)
-                loop_adding_inputs : for i in 0 to g_NB_INPUTS-1 loop           
-                    v_inputs_sum    := v_inputs_sum + to_integer( r_inputs(i) );
-                end loop;
+                output_i : for i in 0 to g_NB_OUTPUTS - 1 loop
+    
+                    v_res           := 0;
 
-                -- outputs(i) = weights(i) * inputs_sum + ( nb_inputs * offsets(i) ) 
-                loop_computing_outputs : for i in 0 to g_NB_OUTPUTS-1 loop      
-                    r_outputs(i)    <= to_signed( r_weights(i) * v_inputs_sum + (g_NB_INPUTS * r_offsets(i)) , p_DATA_WIDTH );
+                    input_j : for j in 0 to g_NB_INPUTS - 1 loop
+
+                        v_res       := v_res + r_inputs(j) * r_weights(i, j);
+
+                    end loop;
+
+                    v_res           := v_res + r_bias(i);
+
+                    r_outputs(i)    <= to_integer(to_signed( v_res, p_DATA_WIDTH ));
+
                 end loop;
 
             end if;
--- hw/ips/parameters/parameters.vhd
+++ hw/ips/parameters/parameters.vhd
@@ -7,30 +7,29 @@ use ieee.numeric_std.all;
 package parameters is
 
     -- NETWORK DATA WIDTH
-    constant p_DATA_WIDTH       : integer := 64;
+    constant p_DATA_WIDTH : integer := 8;
 
     -- TYPES
-    subtype t_data                is signed(p_DATA_WIDTH - 1 downto 0);         -- atomic data element
-    type    t_data_array          is array (integer range <>) of T_DATA;        -- array of data for IP's interal registers
-    type    t_int_array           is array (integer range <>) of integer;       -- array of integers 
+    subtype t_data          is signed(p_DATA_WIDTH - 1 downto 0);                         -- atomic data element
+    type    t_int_array     is array (integer range <>)                   of integer;     -- array of integer
+    type    t_neurons       is array (integer range <>)                   of integer;     -- number of neurons for each layer
+    type    t_bias          is array (integer range <>)                   of integer;     -- array of bias for one layer
+    type    t_weights       is array (integer range <>, integer range <>) of integer;     -- weights(row, column) = weights(output, input)
 
     -- NETWORK PARAMETERS
 
         -- number of inputs of the first layer
-        constant p_NETWORK_INPUTS   : integer                                   := 2;               
+        constant p_NETWORK_INPUTS   : integer                               := 2;               
 
         -- number of layers inside the network
-        constant p_NETWORK_LAYERS   : integer                                   := 5;               
+        constant p_NETWORK_LAYERS   : integer                               := 5;               
 
-        -- nb of weights of each layer
-        constant p_NETWORK_WEIGHTS  : t_int_array(0 to p_NETWORK_LAYERS - 1)    := (4, 3, 2, 4, 4);
+        -- nb of neurons (=outputs) of each layer
+        constant p_NETWORK_NEURONS  : t_neurons(0 to p_NETWORK_LAYERS - 1)  := (4, 3, 2, 4, 4);
 
-        -- must be equal to the number of weights of the last layer
-        constant p_NETWORK_OUTPUTS  : integer                                   := 4;               
+        -- must be equal to the number of neurons of the last layer
+        constant p_NETWORK_OUTPUTS  : integer                               := 4;               
             
-        -- vivado does not support this 
-        -- constant p_NETWORK_OUTPUTS  : integer := (p_NETWORK_WEIGHTS(p_NETWORK_LAYERS - 1));         
-
 end package;
  
 package body parameters is
