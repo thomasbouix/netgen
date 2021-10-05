82bf741fdcec3a7e2f0bd4136137550c5d37e544
Thomas <thomas.bouix1@gmail.com>
2021-10-05 14:53:32 +0200

    fix(network.py) : adding bias

--- hw/ips/generic_fc_nn/tb/network.py
+++ hw/ips/generic_fc_nn/tb/network.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # inputs
-inputs = [1, 1]
+inputs = [1, 0]
 
 # weights [outputs][inputs]
 w0 = [ [1, 1], [1, 1], [1, 1], [1, 1] ]
@@ -11,11 +11,11 @@ w3 = [ [1, 1], [1, 1], [1, 1], [1, 1] ]
 w4 = [ [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1] ]
 
 # bias[outputs]
-b0 = [0, 0, 0, 0]
+b0 = [0, 1, 0, 0]
 b1 = [0, 0, 0]
 b2 = [0, 0]
 b3 = [0, 0, 0, 0]
-b4 = [0, 0, 0, 0]
+b4 = [0, 0, 0, -1]
 
 # neurons output
 n0 = [0, 0, 0, 0]
@@ -31,28 +31,28 @@ def print_list(l) :
 
 def compute_layer_0() :
     for i in range(len(n0)) :               # for each neuron  
-        n0[i] = 0
+        n0[i] = b0[i] 
         for j in range(len(inputs)) :       # for each input
             n0[i] += w0[i][j] * inputs[j]
 def compute_layer_1() :
     for i in range(len(n1)) :               
-        n1[i] = 0
+        n1[i] = b1[i]
         for j in range(len(n0)) :        
             # print("n1[", i,"] += w1[", i,"][", j,"] * n0[", j,"]")
             n1[i] += w1[i][j] * n0[j]
 def compute_layer_2() :
     for i in range(len(n2)) :               
-        n2[i] = 0
+        n2[i] = b2[i]
         for j in range(len(n1)) :        
             n2[i] += w2[i][j] * n1[j]
 def compute_layer_3() :
     for i in range(len(n3)) :               
-        n3[i] = 0
+        n3[i] = b3[i]
         for j in range(len(n2)) :        
             n3[i] += w3[i][j] * n2[j]
 def compute_layer_4() :
     for i in range(len(n4)) :               
-        n4[i] = 0
+        n4[i] = b4[i]
         for j in range(len(n3)) :        
             n4[i] += w4[i][j] * n3[j]
 
