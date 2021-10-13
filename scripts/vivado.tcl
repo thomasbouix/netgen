876ae822a5ff46f568de10440add4f85bea9f40f
Thomas <thomas.bouix1@gmail.com>
2021-10-14 00:15:27 +0200

    fix(vivado.tcl) : reducing pl clock speed from 100MHz to 45MHz

--- scripts/vivado.tcl
+++ scripts/vivado.tcl
@@ -33,6 +33,7 @@ create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_sy
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
 set_property -dict [list CONFIG.PCW_TTC0_PERIPHERAL_ENABLE {0} CONFIG.PCW_USB0_PERIPHERAL_ENABLE {0}] [get_bd_cells processing_system7_0]
 set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_USE_S_AXI_HP1 {1}] [get_bd_cells processing_system7_0]
+set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {45}] [get_bd_cells processing_system7_0]
 connect_bd_net [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK]
 
 ## RESET
@@ -68,8 +69,8 @@ make_wrapper -files [get_files $ROOT_DIR/vivado/vivado.srcs/sources_1/bd/design_
 add_files -norecurse $ROOT_DIR/vivado/vivado.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
 
 # CREATING BITSTREAM
-# launch_runs impl_1 -to_step write_bitstream -jobs 4
-# wait_on_run impl_1
+launch_runs impl_1 -to_step write_bitstream -jobs 4
+wait_on_run impl_1
 
 # HARDWARE EXPORTATION
 # write_hw_platform -fixed -include_bit -force -file $ROOT_DIR/vivado/design_1_wrapper.xsa
